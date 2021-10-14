b443f2b3652ddd47387908445f6ed81a4511a355
devbitfia <86367911+devbitfia@users.noreply.github.com>
2021-10-14 21:10:39 +0530

    Adde Main class to make request with client

---
+++ java-client/Main.java
@@ -0,0 +1,9 @@
+
+
+public class Main {
+	public static void main(String args[]) throws Exception {
+    	Client client = new Client();
+    	client.test();
+    	client.getInvoice("voZ3pYmiE16FLaSfFmytouwFfcjR4Zom8");
+    }
+}
